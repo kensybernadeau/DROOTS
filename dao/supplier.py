e1b77a9c2f6195d3f2c5a8eb0d7e65b450c76690
Vincent0530 <vincent.prado@upr.edu>
2020-05-04 16:58:48 -0400

    Made changes to supplier dao.

--- dao/supplier.py
+++ dao/supplier.py
@@ -4,10 +4,9 @@ import psycopg2
 
 class SupplierDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s host=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
                                                                     pg_config['user'],
-                                                                    pg_config['passwd'],
-                                                                    pg_config['host'])
+                                                                    pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
     def getAllSuppliers(self):
--- dao/users.py
+++ dao/users.py
@@ -5,10 +5,9 @@ import psycopg2
 class UsersDAO:
     def __init__(self):
 
-        connection_url = "dbname=%s user=%s password=%s host=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
-                                                            pg_config['passwd'],
-                                                            pg_config['host'])
+                                                            pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
     def getAllUsers(self):
