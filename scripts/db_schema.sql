c81280c430952490d378e1ad1f6a41ce4001a62c
Vincent0530 <vincent.prado@upr.edu>
2020-05-15 12:04:18 -0400

    Fixed new get for administrator.

--- scripts/db_schema.sql
+++ scripts/db_schema.sql
@@ -6,19 +6,23 @@ CREATE TABLE Customer(customer_id serial PRIMARY KEY, user_id INTEGER REFERENCES
 
 CREATE TABLE Address(address_id serial primary key,user_country varchar(20),user_city varchar(20),user_street_address varchar(50),user_zipcode integer, user_id integer REFERENCES Users(user_id))
 
+create table phone(phone_id serial primary key, phone_number char(12), user_id integer REFERENCES Users(user_id))
+
+create table email(email_id serial primary key, email_address varchar(64), user_id integer REFERENCES Users(user_id))
+
 CREATE TABLE Suppliers(supplier_id serial PRIMARY KEY,user_id INTEGER REFERENCES Users(user_id))
 
-CREATE TABLE  Supplies( supplies_price FLOAT, supplies_stock INTEGER, supplier_id INTEGER REFERENCES Suppliers(supplier_id), resource_id INTEGER REFERENCES Resources(resource_id), PRIMARY KEY(supplier_id, resource_id))
+CREATE TABLE Supplies( supplies_price FLOAT, supplies_stock INTEGER, supplier_id INTEGER REFERENCES Suppliers(supplier_id), resource_id INTEGER REFERENCES Resources(resource_id), PRIMARY KEY(supplier_id, resource_id))
 
-CREATE TABLE  Resources(resource_id  serial PRIMARY KEY, resource_name VARCHAR(20) , resource_category VARCHAR (20))
+CREATE TABLE Resources(resource_id  serial PRIMARY KEY, resource_name VARCHAR(20) , resource_category VARCHAR (20))
 
 CREATE TABLE Ice(ice_id serial PRIMARY KEY, ice_description VARCHAR(250), resource_id INTEGER references resources(resource_id))
 
 CREATE TABLE Water(water_id serial PRIMARY KEY, water_oz INTEGER, water_type VARCHAR(10),resource_id INTEGER references resources(resource_id))
 
-CREATE TABLE  Clothing(clothe_id serial PRIMARY KEY, clothe_type VARCHAR(14) , clothe_size VARCHAR(10) , clothe_description VARCHAR(250) , resource_id INTEGER REFERENCES Resources(resource_id))
+CREATE TABLE Clothing(clothe_id serial PRIMARY KEY, clothe_type VARCHAR(14) , clothe_size VARCHAR(10) , clothe_description VARCHAR(250) , resource_id INTEGER REFERENCES Resources(resource_id))
 
-CREATE TABLE  Tools(tool_id serial PRIMARY KEY, tool_name VARCHAR(10), tool_description VARCHAR(250) , resource_id INTEGER REFERENCES Resources(resource_id))
+CREATE TABLE Tools(tool_id serial PRIMARY KEY, tool_name VARCHAR(10), tool_description VARCHAR(250) , resource_id INTEGER REFERENCES Resources(resource_id))
 
 CREATE TABLE Health(health_id serial PRIMARY KEY, health_type VARCHAR(15), health_exp_date CHAR(10), health_description VARCHAR(250), resource_id  INTEGER REFERENCES resources(resource_id))
 
