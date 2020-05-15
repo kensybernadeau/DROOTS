925f5928c7c0fb264b2ace61a7adb1aa9eab0f61
Rafael444 <rafael.lopez16@upr.edu>
2020-05-15 12:36:53 -0400

    Update supplier and customer

--- dao/supplier.py
+++ dao/supplier.py
@@ -5,15 +5,17 @@ import psycopg2
 class SupplierDAO:
     def __init__(self):
         connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
-                                                                    pg_config['user'],
-                                                                    pg_config['passwd'])
+                                                            pg_config['user'],
+                                                            pg_config['passwd'])
+
         self.conn = psycopg2._connect(connection_url)
 
     def getAllSuppliers(self):
         cursor = self.conn.cursor()
-        query = "select supplier_id, user_id, user_first_name, user_last_name, user_uname, user_password, address_id, " \
-                "user_country, user_city, user_street_address, user_zipcode " \
-                "from suppliers natural inner join users natural inner join address;"
+        query = "select supplier_id, user_id, user_first_name, user_last_name, user_uname, user_password, " \
+                "user_country, user_city, user_street_address, user_zipcode, phone_number, email_address " \
+                "from suppliers natural inner join users natural inner join address natural inner join " \
+                "phone natural inner join email;"
         cursor.execute(query)
         result = []
         for row in cursor:
@@ -22,9 +24,10 @@ class SupplierDAO:
 
     def getSupplierById(self, supplier_id):
         cursor = self.conn.cursor()
-        query = "select supplier_id, user_id, user_first_name, user_last_name, user_uname, user_password, address_id, " \
-                "user_country, user_city, user_street_address, user_zipcode " \
-                "from suppliers natural inner join users natural inner join address where supplier_id = %s;"
+        query = "select supplier_id, user_id, user_first_name, user_last_name, user_uname, user_password, " \
+                "user_country, user_city, user_street_address, user_zipcode, phone_number, email_address " \
+                "from suppliers natural inner join users natural inner join address natural inner join phone" \
+                " natural inner join email where supplier_id = %s;"
         cursor.execute(query, (supplier_id,))
         result = cursor.fetchone()
         return result
